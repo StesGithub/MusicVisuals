1b9e35ab54399e85e349a3e1bbb67df3cb2d78a6
marc-rene <95175463+marc-rene@users.noreply.github.com>
2023-04-30 00:16:37 +0100

    Removed Old dead code
    Removed references to the sketch select.java as its no longer needed

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -1,7 +1,6 @@
 package ie.tudublin;
 
 import c21394693.longWalkHome;
-//import c21415142.Cesar_2;
 import example.CubeVisual;
 import example.MyVisual;
 import example.RotatingAudioBands;
@@ -27,65 +26,5 @@ public class Main {
 
 		longWalkHome game = new longWalkHome();
 		System.out.println("Start");
-		/*Thread option_menu = new Thread(new show_option_menu());
-		option_menu.start();
-		option_menu.join();
-
-		System.out.println("End");
- 
-		switch (visual_to_run) {
-			case 0: // Stephens Long walk home
-				System.out.println("Doing " + visual_to_run);
-				longWalkHome game = new longWalkHome();
-				break;
-
-			case 1: // Stephens 2nd thing
-				System.out.println("Doing " + visual_to_run);
-				longWalkHome game_2 = new longWalkHome();
-				break;
-
-			case 2: // Césars First visual
-				System.out.println("Doing " + visual_to_run);
-				processing.core.PApplet.runSketch(arg, new Cesar_2());
-				break;
-				
-			case 3: // César's second visual
-				System.out.println("Doing " + visual_to_run);
-				processing.core.PApplet.runSketch(arg, new Cesar_2());
-				break;
-
-			default:
-				System.out.println("Doing " + visual_to_run);
-				System.out.println("ERROR in switch");
-				break;
-		}
-
 	}
-*/}
 }
-/* 
-class show_option_menu implements Runnable {
-	public void run() {
-		Sketch_Select option_menu = new Sketch_Select();
-		
-		processing.core.PApplet.runSketch(Main.arg, option_menu);
-
-		while (true) {
-			Main.visual_to_run = option_menu.return_choice();
-
-			if (Main.visual_to_run < 0 || Main.visual_to_run > 7) {
-				// user hasn't chosen anything
-				System.out.println("Reading " + Main.visual_to_run);
-				continue;
-			}
-
-			else {
-				// Something has been chosen
-				option_menu.noLoop();
-				option_menu.getSurface().setVisible(false);
-				option_menu.getSurface().stopThread();
-				break;
-			}
-		}
-	}
-}*/
